55107d2b2155f781b73431bdc589579762339d1b
silcreteantelope <ballmax23@gmail.com>
2021-04-22 15:44:46 -0400

    Changed how flags are accepted, it now accepts flags like this -cpi

--- main.cpp
+++ main.cpp
@@ -16,11 +16,12 @@ string getUserFile(){
 		cin >> filename;
 		filenameLength = filename.length();	//record its length
 		for(int i=0; i<filenameLength; i++) {	//make sure it doesn't contain any illegal characters
-			if(illegalChars.find_first_of(filename[i]) != string::npos) hasIllegal = true;
+			cout<<"Current char: "<<filename[i]<<"\n";
+			if(illegalChars.find_first_of(filename[i]) != string::npos){ hasIllegal = true; cout<<"This char is illegal"<<filename[i]<<"\n";}
 			else if(!isalnum(filename[i])) hasIllegal = true; 	//if it does, ask for a new one
 		}
 
-        cout << "THe filename you entered was not valid." << endl; //repeats the process of prompting for a name until the user enters a valid one
+        cout << "The filename you entered was not valid." << endl; //repeats the process of prompting for a name until the user enters a valid one
         cin.clear();    //clears the previous input in the case it wasn't a valid filename
         cin.ignore(256,'\n');   //ignores the previous enter
     } while((cin.fail()) || hasIllegal || (filenameLength >= maxFilenameLength));
@@ -32,7 +33,7 @@ string getUserFile(){
 }
 
 bool isattemptAtInjection(string input){
-
+	return true; //Temp fix, Sean fix your shit their needs to be a return statement
 }
 
 //Int copy() takes src, path, and flags and copies the file to the path
@@ -72,7 +73,7 @@ int copy(string src,string path,int cflag,int pflag,int iflag){
 
 
 int main(int arc, char* argv[]) {
-	cout << "\nFileCopy started: ";
+	cout << "\nFileCopy started: \n";
 	
 	string src, dest, path, input;
 	int checksum=0,permission=0,info=0;
@@ -103,19 +104,22 @@ int main(int arc, char* argv[]) {
 			info=1;
 		}
 	}
-	for(int i=0;i<=arc-1;i++){
+	for(int i=1;i<=arc-1;i++){
+		cout << "argv["<< i << "]= " << argv[i] << "\n";
 		string temp(argv[i]);
-		if(temp=="checksum" || temp=="c"){
-			cout << "checksum flag active\n";
-			checksum=1;
-		}				
-		else if(temp=="permission"|| temp=="p"){
-			cout << "permission flag active\n";
-			permission=1;
-		}
-		else if(temp=="info"|| temp=="i"){
-			cout << "info text file flag active\n";
-			info=1;
+		if(temp.find('-') != std::string::npos){
+			if(temp.find('c') != std::string::npos){
+				cout << "checksum flag active\n";
+				checksum=1;
+			}
+			if(temp.find('p') != std::string::npos){
+				cout << "permission flag active\n";
+				permission=1;
+			}
+			if(temp.find('i') != std::string::npos){
+				cout << "info text file flag active\n";
+				info=1;
+			}
 		}
 		else {
 			if(src.empty())
@@ -124,6 +128,51 @@ int main(int arc, char* argv[]) {
 				string destpath(argv[i]);
 				path=destpath+"/"+src;
 			}
+		}
+	}
+	if(src.empty()){
+		cout << "Enter src file to copy: ";
+		src = getUserFile();
+		cout << "Would you like to checksum the src and moved files to check file integrity(y/n) ";
+		cin >> input;
+		if(input=="Y"||input=="y"){
+			cout << "checksum flag active\n";
+			checksum=1;
+		}
+		cout << "Would you like to change the owner and group permissions of file(y/n) ";
+		cin >> input;
+		if(input=="Y"||input=="y"){
+			cout << "permission flag active\n";
+			permission=1;
+		}
+		cout << "Would you like their to be a .txt file in the destination path with";
+		cout << "time, date, and original file path of the file transfer for future notice(y/n) ";
+		cin >> input;
+		if(input=="Y"||input=="y"){
+			cout << "info text file flag active\n";
+			info=1;
+		}
+	}
+	if(path.empty()){
+		cout << "Enter full destination path to copy src file to: ";
+		dest = getUserFile();
+		path=dest+"/"+src;
+		if(input=="Y"||input=="y"){
+			cout << "checksum flag active\n";
+			checksum=1;
+		}
+		cout << "Would you like to change the owner and group permissions of file(y/n) ";
+		cin >> input;
+		if(input=="Y"||input=="y"){
+			cout << "permission flag active\n";
+			permission=1;
+		}
+		cout << "Would you like their to be a .txt file in the destination path with";
+		cout << "time, date, and original file path of the file transfer for future notice(y/n) ";
+		cin >> input;
+		if(input=="Y"||input=="y"){
+			cout << "info text file flag active\n";
+			info=1;
 		}
 	}
 	copy(src,path,checksum,permission,info);
